ca8ff5f810690dcc9d5094fd05d75088895f7391
Damon Barry <damonb@microsoft.com>
2016-08-19 23:06:04 -0700

    set keep-alive options, then wait for 15 seconds

--- sample/main.c
+++ sample/main.c
@@ -1,5 +1,5 @@
-#include <windows.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include "azure_c_shared_utility/threadapi.h"
 #include "azure_c_shared_utility/platform.h"
 #include "azure_c_shared_utility/tlsio.h"
@@ -9,10 +9,18 @@ int exit_value = -1;
 
 void on_io_open_complete(void* context, IO_OPEN_RESULT open_result)
 {
-    (void)context;
-
     if (open_result == IO_OPEN_OK) printf("connection is open!\n");
     else printf("error!\n");
+
+    int onoff = 1;
+    int time = 5;
+    int interval = 5;
+    int result = xio_setoption(context, "tcp_keepalive", &onoff);
+    if (!!result) printf("error (%d) setting tcp_keepalive\n", result);
+    result = xio_setoption(context, "tcp_keepalive_time", &time);
+    if (!!result) printf("error (%d) setting tcp_keepalive\n", result);
+    result = xio_setoption(context, "tcp_keepalive_interval", &interval);
+    if (!!result) printf("error (%d) setting tcp_keepalive\n", result);
 
     exit_value = open_result;
     should_quit = true;
@@ -20,6 +28,8 @@ void on_io_open_complete(void* context, IO_OPEN_RESULT open_result)
 
 int main()
 {
+    size_t counter = SIZE_MAX;
+
     if (platform_init() == 0) {
         TLSIO_CONFIG tls_io_config = { "google.com", 443 };
         const IO_INTERFACE_DESCRIPTION* tlsio_interface = platform_get_default_tlsio();
@@ -27,7 +37,9 @@ int main()
         XIO_HANDLE tls_io = xio_create(tlsio_interface, &tls_io_config);
         if (tls_io != NULL) {
             if (xio_open(tls_io, on_io_open_complete, tls_io, NULL, NULL, NULL, NULL) == 0) {
-                while (!should_quit) {
+                while (!should_quit || counter > 0) {
+                    if (should_quit && counter < SIZE_MAX) --counter;
+                    if (should_quit && counter == SIZE_MAX) counter = 15;
                     xio_dowork(tls_io);
                     ThreadAPI_Sleep(1000);
                 }
